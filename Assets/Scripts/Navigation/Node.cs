3b5ae0b7929c18f715d153e0ffea27929cd7ca75
Farry1 <mail@lukasfabry.de>
2019-10-18 15:25:12 +0200

    started degree lock thingy for pushback

--- Assets/Scripts/Navigation/Node.cs
+++ Assets/Scripts/Navigation/Node.cs
@@ -63,21 +63,100 @@ public class Node : MonoBehaviour
 
     public static Vector3 GetOppositePlanarDirection(Node currentNode, Node targetNode)
     {
-        currentNode.HighlightField(Color.yellow);
-
-        Debug.Log(currentNode.name);
 
         Vector3 origin = currentNode.transform.position;
         Vector3 destination = targetNode.transform.position;
 
-        targetNode.HighlightField(Color.green);
 
 
 
         Vector3 direction = origin - destination;
 
-        Debug.DrawRay(origin, direction, Color.red, 5f);
+        Debug.DrawRay(origin, currentNode.transform.up, Color.blue, 2f);
+
+        Vector3 planarDirection = Vector3.ProjectOnPlane(direction, currentNode.transform.up);
+
+        planarDirection = planarDirection.normalized;
+        Color debugColor = Color.red;
+
+        planarDirection = LockAnglesOn45Degrees(planarDirection);
+
+
+        Debug.DrawRay(origin, planarDirection, debugColor, 5f);
 
         return direction;
     }
+
+    public static Vector3 LockAnglesOn45Degrees(Vector3 direction)
+    {
+
+        Debug.Log(direction);
+
+        Vector3 lockedDirection = Vector3.zero;
+
+        if (direction.x <= -0.9)
+        {
+            lockedDirection += Vector3.right * -1;
+        }
+
+
+
+        //if (planarDirection.x <= -0.9)
+        //{
+        //    debugColor = Color.yellow;
+        //    planarDirection = -currentNode.transform.right;
+
+        //}
+        //else if ((planarDirection.x > -0.9) && (planarDirection.x <= -0.3))
+        //{
+
+        //    if (planarDirection.z > 0)
+        //    {
+        //        planarDirection = currentNode.transform.right * -1 + currentNode.transform.forward;
+        //        debugColor = Color.green;
+        //    }
+        //    else
+        //    {
+        //        //planarDirection = currentNode.transform.right * -1 + currentNode.transform.forward;
+        //    }
+
+        //}
+        //else if ((planarDirection.x > -0.3) && (planarDirection.x <= 0.3))
+        //{
+
+        //    if (planarDirection.z > 0)
+        //    {
+        //        //planarDirection = new Vector3(0f, 0f, 1f);
+        //    }
+        //    else
+        //    {
+        //        //planarDirection = new Vector3(0f, 0f, -1f);
+        //    }
+
+        //}
+        //else if ((planarDirection.x > 0.3) && (planarDirection.x <= 0.9))
+        //{
+
+        //    if (planarDirection.z > 0)
+        //    {
+        //        //planarDirection = new Vector3(0.7f, 0f, 0.7f);
+        //    }
+        //    else
+        //    {
+        //        //planarDirection = new Vector3(0.7f, 0f, -0.7f);
+        //    }
+
+        //}
+        //else if (planarDirection.x > 0.9)
+        //{
+
+        //    //planarDirection = new Vector3(1f, 0f, 0f);
+        //}
+
+        // planarDirection = currentNode.transform.localRotation * planarDirection;
+
+
+        return direction;
+    }
+
 }
